524a4777a61f014d1902fd18bb15bc0cb137a1e5
vaivk4 <vai.vk4@gmail.com>
2021-02-08 00:43:56 +0530

    Assignment1 done

---
+++ Data Structures and Algorithm with Python Essentials/assignment1/question1.py
@@ -0,0 +1,16 @@
+#Question 1 Write a python program that takes two numbers as the input and print the result of Addition,Subtraction, Multiplication, Division of the two numbers
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+#Addition
+print("addition = ",num1+num2)
+
+#Subtraction
+print("Subtraction = ",num1-num2)
+
+#Multiplication
+print("Multiplication = ",num1*num2)
+
+#division
+print("division = ",num1/num2)
---
+++ Data Structures and Algorithm with Python Essentials/assignment1/question2.py
@@ -0,0 +1,13 @@
+#Question 2: Write a python program that takes two numbers as the input such as X and Y and print the result of X^Y(X to the power of Y).
+
+#python program to perform power operation
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+#power of A^B
+print("power is:",num1**num2)#python program to perform power operation
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+#power of A^B
+print("power is:",num1**num2)
